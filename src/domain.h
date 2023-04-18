092f13e282aa0ed38136bb44777fc17c060a106d
Ivan <xx@ya.ru>
2023-04-18 23:40:35 +0300

    sort settings filds

--- src/domain.h
+++ src/domain.h
@@ -10,11 +10,12 @@ namespace settings {
 struct Settings {
 
   Abonent internal_abonent_;
+  InterfaceSettings lan_settings;
+  InterfaceSettings inet_settings;
   std::vector<Abonent> abonents_;
   std::vector<ArpAddress> arp_abonents_;
   std::optional<std::uint8_t> time;
+  std::optional<std::uint8_t> devicenumber;
   std::optional<std::uint8_t> protocol;
-  InterfaceSettings lan_settings;
-  InterfaceSettings inet_settings;
 };
 } // namespace settings
