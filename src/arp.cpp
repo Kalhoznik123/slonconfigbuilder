3efb5fc2c709bb5ff391143c8a095ff6b3b2708a
Ivan <xx@ya.ru>
2023-04-19 23:36:42 +0300

    change arp

--- src/arp.cpp
+++ src/arp.cpp
@@ -1,1 +1,11 @@
 #include "arp.h"
+
+ std::string ArpAddress::Address() const { return arp_address_; }
+
+
+std::uint8_t ArpAddress::Number() const { return number_; }
+
+std::string ArpAddress::ToString() const {
+  //TODO: добавить реализацию
+  
+   return std::string(); }
--- src/arp.h
+++ src/arp.h
@@ -4,11 +4,16 @@
 class ArpAddress {
 
 public:
- ArpAddress() = default;
- 
- 
+  ArpAddress() = default;
+
   ArpAddress(std::uint8_t number, const std::string& arp_address)
       : number_(number), arp_address_(arp_address) {}
+
+  std::string Address() const;
+
+  std::uint8_t Number() const;
+
+  std::string ToString() const;
 
 private:
   std::uint8_t number_;
