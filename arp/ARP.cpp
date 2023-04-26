0c42106c0b8a8cad7f0bb0ce6aecc5beb2e189d2
Ivan <xx@ya.ru>
2023-04-26 23:49:18 +0300

    add arp like class

---
+++ arp/ARP.cpp
@@ -0,0 +1,44 @@
+#include "ARP.h"
+#include <exception>
+#include <regex>
+
+
+namespace network {
+
+ARP::ARP(const std::string& mask) {
+
+  if (IsValid(mask)) {
+    address_ = Parse(mask);
+
+  } else {
+    throw std::invalid_argument("invalid ip mask");
+  }
+}
+
+bool ARP::IsValid(const std::string& mask) const {
+
+  const std::regex pattern("^([0-9A-Fa-f]{2}[:-]){5}"
+                           "([0-9A-Fa-f]{2})|([0-9a-"
+                           "fA-F]{4}\\.[0-9a-fA-F]"
+                           "{4}\\.[0-9a-fA-F]{4})|([0-9A-Fa-f]{12})$");
+
+  // Return true if the MAC address
+  // matched the ReGex
+  return std::regex_match(mask, pattern);
+}
+
+std::string ARP::ToString() const { return address_; }
+
+std::string ARP::Parse(const std::string& mask) const {
+
+  const std::regex re(DELIMITERS);
+  std::sregex_token_iterator first{std::begin(mask), std::end(mask), re, -1},
+      last; // the '-1' is what makes the regex split (-1 := what was not
+            // matched)
+  std::vector<std::string> tokens{first, last};
+
+  const std::string result = Join(first, last);
+
+  return result;
+}
+} // namespace network
---
+++ arp/ARP.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+
+
+
+namespace network {
+
+class ARP {
+
+public:
+  explicit ARP(const std::string& mask);
+
+  std::string ToString() const;
+
+private:
+  std::string address_;
+  bool IsValid(const std::string& mask) const;
+
+  std::string Parse(const std::string& mask) const;
+
+  template <typename Iterator>
+  std::string Join(Iterator first, Iterator last) const {
+
+    std::string result;
+
+    for (; first != last; ++first) {
+      result += *first;
+    }
+
+    return result;
+  }
+
+  const std::string DELIMITERS = "[\\.,:-]";
+};
+
+} // namespace network
