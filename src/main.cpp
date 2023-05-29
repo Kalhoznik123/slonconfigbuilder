3d98960888e9091c191274efcfbc2906ac9763d3
Ivan <xx@ya.ru>
2023-05-30 00:22:20 +0300

    add new functional

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,49 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <boost/program_options.hpp>
+#include "IBuilder.h"
 #include "config_builder.h"
 #include "json_builder.h"
+#include <boost/program_options.hpp>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
 
+// using namespace boost::program_options;
 
-int main(int, char**) {
+int main(int argc, char** argv) {
 
+  boost::program_options::options_description desc{"Options"};
 
-//TODO: попробовать совместить с передачей по компорту
+  desc.add_options()("help,h", "Help decsription")(
+      "input-file,I", boost::program_options::value<std::string>(),
+      "Path to settings file")("interactive,i", "Interactive mode");
 
+  boost::program_options::variables_map vm;
+  boost::program_options::store(parse_command_line(argc, argv, desc), vm);
+  boost::program_options::notify(vm);
 
-JsonBuilder builder(std::cin);
+  // TODO: попробовать совместить с передачей по компорту
 
-const auto settings  = builder.MakeSettings();
+  // JsonBuilder builder(std::cin);
+  std::unique_ptr<IBuilder> builder;
 
-const configurator::ConfigBuilder config_builder(settings);
+  settings::Settings settings;
 
-std::ofstream file("config.txt", std::ios::out);
+  if (const auto it = vm.find("help"); it != vm.end()) {
+    std::cout << desc << std::endl;
+    exit(0);
+  }
+  if (const auto it = vm.find("input-file"); it != vm.end()) {
+    std::ifstream in_file(it->second.as<std::string>(), std::ios::in);
+    std::unique_ptr<IBuilder> builder = std::make_unique<JsonBuilder>(in_file);
 
-file << config_builder.Dump() << std::endl;
+    settings = builder->MakeSettings();
+  } else if (const auto it = vm.find("interactive"); it != vm.end()) {
+    std::cout << "interactive mode" << std::endl;
+  }
 
+  const configurator::ConfigBuilder config_builder(settings);
+
+  std::ofstream file("config.txt", std::ios::out);
+
+  file << config_builder.Dump() << std::endl;
 }
