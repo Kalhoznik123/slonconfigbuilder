b93097ae2caef92f1cded30710b2337d92fdb15a
Ivan <xx@ya.ru>
2023-11-28 23:44:53 +0300

    update abonent

--- src/abonent.cpp
+++ src/abonent.cpp
@@ -6,16 +6,13 @@ const std::string& Abonent::Address() const noexcept { return address_; }
 
 const IP_Mask& Abonent::Mask() const noexcept { return mask_; }
 
-const std::optional<int>& Abonent::Devicenumber() const noexcept {
-    return number_;
-}
 
 std::string Abonent::ToString() const {
 
     std::stringstream str_stream;
 
-    if (number_)
-        str_stream << *number_ << " ";
+    //if (number_)
+      //  str_stream << *number_ << " ";
 
     str_stream << address_ << " " ;
 
@@ -27,4 +24,4 @@ std::string Abonent::ToString() const {
     return str_stream.str();
 }
 
-const AbonentType& Abonent::AbonType() const noexcept { return abonent_type_; }
+//const AbonentType& Abonent::AbonType() const noexcept { return abonent_type_; }
--- src/abonent.h
+++ src/abonent.h
@@ -6,26 +6,24 @@
 
 enum class AbonentType { INTERNAL, REMOTE };
 
-class Abonent {
+class Abonent  {
 
 public:
   Abonent() = default;
 
   Abonent(const std::string& address,
-          const IP_Mask& mask, AbonentType abonent_type,
-          const std::optional<int>& number = std::nullopt)
-      : address_(address), mask_(mask), abonent_type_(abonent_type),
-        number_(number) {}
+          const IP_Mask& mask )
+      : address_(address), mask_(mask){}
 
   const std::string& Address() const noexcept;
-  const IP_Mask& Mask() const noexcept;
-  const std::optional<int>& Devicenumber() const noexcept;
-  std::string ToString() const;
-  const AbonentType& AbonType() const noexcept;
+  const IP_Mask& Mask() const noexcept;  
+  std::string ToString() const;  
 
 private:
   std::string address_;
   IP_Mask mask_;
-  AbonentType abonent_type_;
-  std::optional<int> number_;
+
+
 };
+
+
