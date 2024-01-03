b2d465bac14be2c7a5671ed7dea19f91a87e2dea
Ivan <xx@ya.ru>
2024-01-03 16:25:48 +0300

    add new file

---
+++ src/yaml_builder.cpp
@@ -0,0 +1,6 @@
+#include "yaml_builder.h"
+
+yaml_builder::yaml_builder()
+{
+
+}
---
+++ src/yaml_builder.h
@@ -0,0 +1,11 @@
+#ifndef YAML_BUILDER_H
+#define YAML_BUILDER_H
+
+
+class yaml_builder
+{
+public:
+    yaml_builder();
+};
+
+#endif // YAML_BUILDER_H
