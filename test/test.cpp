964406f5d751fa040c7abd2044d0d00f7f8a863a
Ivan <xx@ya.ru>
2024-02-29 00:34:28 +0300

    refactor: add new tests and update old

--- test/test.cpp
+++ test/test.cpp
@@ -13,6 +13,9 @@
 #include "yaml_builder.h"
 #include "parsers.h"
 
+
+
+
 class AbonentTest : public ::testing::Test
 {
 protected:
@@ -307,7 +310,7 @@ TEST(ParserInterfaceTest,ParseWhithSpaces){
 
     //assert
 
-    ASSERT_EQ(ok,true);
+    ASSERT_TRUE(ok);
 }
 
 
@@ -335,18 +338,23 @@ TEST(ParserInterfaceTest,Result){
 
 TEST(ParserInternalAbonentTest,IP_AdressResult){
     //arrange
-
+    using parser = parsers::internal_abonent_parser::internal_abonent_parser<std::string::const_iterator>;
     parsers::internal_abonent_parser::IAbonentParseRes res;
+    parsers::internal_abonent_parser::IAbonentParseRes res2;
+    parsers::internal_abonent_parser::IAbonentParseRes res3;
+
     const std::string value = "192.168.3.0 24";
-
+    const std::string value2 = "22.22.33.0 24";
+    const std::string value3 = "1.1.123.0 24";
     //act
-    parsers::Parse<parsers::internal_abonent_parser::internal_abonent_parser<std::string::const_iterator>>(value,res);
-
-
+    parsers::Parse<parser>(value,res);
+    parsers::Parse<parser>(value2,res2);
+    parsers::Parse<parser>(value3,res3);
     //assert
 
     ASSERT_EQ(res.ip_address,std::string("192.168.3.0"));
-
+    ASSERT_EQ(res2.ip_address,std::string("22.22.33.0"));
+    ASSERT_EQ(res3.ip_address,std::string("1.1.123.0"));
 }
 
 TEST(ParserInternalAbonentTest,MaskIntType){
@@ -364,6 +372,29 @@ TEST(ParserInternalAbonentTest,MaskIntType){
     ASSERT_EQ(boost::get<int>(res.mask),24);
 
 }
+
+TEST(ParserInternalAbonentTest,ParseFail){
+    //arrange
+    using parser = parsers::internal_abonent_parser::internal_abonent_parser<std::string::const_iterator>;
+    parsers::internal_abonent_parser::IAbonentParseRes res;
+    parsers::internal_abonent_parser::IAbonentParseRes res2;
+    parsers::internal_abonent_parser::IAbonentParseRes res3;
+    const std::string value = "192.12368.1.0 24";
+    const std::string value2 = "112312392.168.2.0 24";
+    const std::string value3 = "192.16453453458.2.0 24";
+    //act
+    bool ok = parsers::Parse<parser>(value,res);
+    bool ok2 = parsers::Parse<parser>(value2,res2);
+    bool ok3 = parsers::Parse<parser>(value3,res3);
+    //p
+
+    //assert
+
+    ASSERT_FALSE(ok);
+    ASSERT_FALSE(ok2);
+    ASSERT_FALSE(ok3);
+}
+
 
 TEST(ParserInternalAbonentTest,MaskStringType){
     //arrange
