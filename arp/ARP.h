916b0aad82a3463614831b3924d3e5402c74b52b
Ivan <dr.kalhoznik@yandex.ru>
2024-07-09 23:24:50 +0300

    refactor:some beutify

--- arp/ARP.h
+++ arp/ARP.h
@@ -9,27 +9,26 @@ const std::string DELIMITERS = "[\\.,:-]";
 class ARP {
 
 public:
-    explicit ARP(std::string_view mask);
+  explicit ARP(std::string_view mask);
 
-    const std::string& Address() const;
+  const std::string &Address() const;
 
 private:
-    std::string address_;
+  bool IsValid(std::string_view mask) const;
 
-    bool IsValid(std::string_view mask) const;
+  std::string Parse(std::string mask) const;
 
-    std::string Parse(std::string mask) const;
-
-    template <typename Iterator>
-    std::string Join(Iterator first, Iterator last) const {
-
-        std::string result;
-        for (; first != last; ++first) {
-            result += *first;
-        }
-        return result;
+  template <typename Iterator>
+  std::string Join(Iterator first, Iterator last) const {
+    std::string result;
+    for (; first != last; ++first) {
+      result += *first;
     }
+    return result;
+  }
 
+private:
+  std::string address_;
 };
 
 } // namespace network
