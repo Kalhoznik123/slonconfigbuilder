80bdb5dd7657f27147f2313851d3ba174b18b3a8
Ivan <dr.kalhoznik@yandex.ru>
2024-07-09 23:47:34 +0300

    refact: some fix

--- src/ip_address.cpp
+++ src/ip_address.cpp
@@ -1,10 +1,7 @@
 #include "ip_address.h"
 
-void IP_address::Address(std::string_view ip_address){
-    address_ = boost::asio::ip::address_v4::from_string(std::string(ip_address));
+void IP_addres::Address(std::string_view ip_address) {
+  addres_ = boost::asio::ip::address_v4::from_string(std::string(ip_address));
 }
 
-std::string IP_address::Address() const{
-
-    return address_.to_string();
-}
+std::string IP_addres::Address() const { return addres_.to_string(); }
--- src/ip_address.h
+++ src/ip_address.h
@@ -1,22 +1,32 @@
 #pragma once
+#include <boost/asio/ip/address_v4.hpp>
 #include <string>
 #include <string_view>
-#include <boost/asio/ip/address_v4.hpp>
 
-class IP_address
-{
+class IP_addres {
 public:
+  IP_addres() = default;
+  IP_addres(const IP_addres &rhs) : addres_(rhs.addres_){};
+  IP_addres(IP_addres &&rhs) noexcept : addres_(std::move(rhs.addres_)){};
+  IP_addres(std::string_view ip_address)
+      : addres_(
+            boost::asio::ip::address_v4::from_string(std::string(ip_address))) {
+  }
+  ~IP_addres() = default;
 
-IP_address() = default;
-IP_address(const IP_address& rhs) = default;
-IP_address(std::string_view ip_address)
-    :address_(boost::asio::ip::address_v4::from_string(std::string(ip_address))){ }
+  IP_addres &operator=(const IP_addres &rhs) {
+    addres_ = rhs.addres_;
+    return *this;
+  }
 
-void Address(std::string_view ip_address);
-std::string Address() const;
+  IP_addres &operator=(IP_addres &&rhs) noexcept {
+    addres_ = std::move(rhs.addres_);
+    return *this;
+  }
+
+  void Address(std::string_view ip_address);
+  std::string Address() const;
 
 private:
-boost::asio::ip::address_v4 address_;
+  boost::asio::ip::address_v4 addres_;
 };
-
-
